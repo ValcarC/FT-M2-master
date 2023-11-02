fb58e74478bd771b27e4861f01b2a0c94fd6791f
ValcarC <valcar.cortes@gmail.com>
2023-11-02 17:02:18 -0600

    hw 06-React 02 Integration Done

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,5 +1,24 @@
 import Card from './Card';
 
-export default function Cards(props) {
-   return <div></div>;
+export default function Cards({characters}) {
+   return (
+      <div>
+      {characters.map((char) => (
+         // console.log(characters)
+   <Card
+         key={char.id}
+         id={char.id}
+         name={char.name}
+         status={char.status}
+         species={char.species}
+         gender={char.gender}
+         origin={char.origin.name}
+         image={char.image}
+         onClose={() => alert('Emulamos que se cierra la card')}
+         />
+         )
+      )}
+   </div>
+   )
+   
 }
